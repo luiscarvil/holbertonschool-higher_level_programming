99d29e03b9b09debed316eba9b97b261a9e67066
luiscarvil <1888@holbertonschool.com>
2020-09-01 13:19:05 -0500

    condicionals in python

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,16 +3,14 @@ import random
 number = random.randint(-10000, 10000)
 
 if number % 10 == 0:
-    print("Last digit of", end=" ")
-    print("{:d} is {:d} and is 0".format(number, number % 10))
+    print("Last digit of {:d} is {:d} and is 0".format(number, number % 10))
 elif number > 0:
-    print("Last digit of", end=" ")
     if number % 10 > 5:
-        print("{:d} is {:d}greater than 5".format(number, number % 10))
+        print("Last digit of {:d} is {:d} greater\
+ than 5".format(number, number % 10))
     elif number % 10 < 6:
-        print("{:d} is {:d}".format(number, number % 10), end=" ")
-        print("and is less than 6 and not 0")
+        print("Last digit of {:d} is {:d} and is less\
+ than 6 and not 0".format(number, number % 10))
 else:
-    print("Last digit of", end=" ")
-    print("{:d} is {:d}".format(number, number % -10), end=" ")
-    print("and is less than 6 and not 0")
+    print("Last digit of {:d} is {:d} and is less\
+ than 6 and not 0".format(number, number % -10))
