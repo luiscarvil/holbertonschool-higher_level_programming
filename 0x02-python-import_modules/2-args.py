feedfc6c6dda0f6074d89ef0c56de0cd3c39adc5
luiscarvil <1888@holbertonschool.com>
2020-09-03 17:26:20 -0500

    add arguments argv and count

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,6 +5,10 @@ if len(argv) - 1 == 0:
     print("0 arguments.")
 else:
     aux = len(argv) - 1
-    print("{} arguments:".format(aux))
-    for i in range(1, aux + 1):
-        print("{}: {}".format(i, sys.argv[i]))
+    if aux == 1:
+        print("{} argument:".format(aux))
+        print("{}: {}".format(aux, sys.argv[aux]))
+    else:
+        print("{} arguments:".format(aux))
+        for i in range(1, aux + 1):
+            print("{}: {}".format(i, sys.argv[i]))
