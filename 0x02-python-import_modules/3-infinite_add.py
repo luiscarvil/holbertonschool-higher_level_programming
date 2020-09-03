3f0f97f09b354746e5a164eae7615cfaed272138
luiscarvil <1888@holbertonschool.com>
2020-09-03 17:56:41 -0500

    add arguments argv

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    from sys import argv
+    suma = 0
+    for i in range(1, len(argv)):
+        suma += int(argv[i])
+    print(suma)
+ 
