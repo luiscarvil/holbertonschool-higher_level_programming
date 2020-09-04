114c9530f83de6a69fc1f7a955c33fbd6bf44153
luiscarvil <1888@holbertonschool.com>
2020-09-03 22:51:08 -0500

    import functions calculator other file with argv

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+from sys import argv, exit
+if len(argv) != 4:
+    print(len(argv))
+    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+    exit(1)
+else:
+    """print("here {}".format(argv[1]))
+    """
+    a = int(argv[1])
+    """print("here {}".format(argv[3]))
+    """
+    b = int(argv[3])
+    if argv[2] == "+":
+        print("{} + {} = {}".format(a, b, add(a, b)))
+    if argv[2] == "-":
+        print("{} - {} = {}".format(a, b, sub(a, b)))
+    if argv[2] == "*":
+        print("{} * {} = {}".format(a, b, mul(a, b)))
+    if argv[2] == "/":
+        print("{} / {} = {}".format(a, b, div(a, b)))
+"""print(len(argv))
+"""
