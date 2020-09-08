59382e96f6b79bd3da4b761c7002882633803642
luiscarvil <1888@holbertonschool.com>
2020-09-08 11:20:28 -0500

    new element in a copy list

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    new_list = my_list.copy()
+    i = len(my_list)
+    if idx < 0:
+        return my_list
+    elif idx > i and idx != range(i):
+        return my_list
+    else:
+        new_list[idx] = element
+        return new_list
