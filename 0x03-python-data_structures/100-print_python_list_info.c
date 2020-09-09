73d220d2084a8fc3610732487a87d5da93a57313
luiscarvil <1888@holbertonschool.com>
2020-09-08 22:47:06 -0500

    print a list in python

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <Python.h>
+
+/**
+ * print_python_list_info - prints python lists
+ * @p: pyobject
+ */
+void print_python_list_info(PyObject *p)
+{
+	int i = 0;
+
+	printf("[*] Size of the Python List = %zd\n", Py_SIZE(p));
+	printf("[*] Allocated = %zd \n", ((PyListObject *)(p))->allocated);
+	for (i = 0; i < Py_SIZE(p); i++)
+		printf("Element %d: %s\n", i, Py_TYPE(PyList_GetItem(p, i))->tp_name);
+}
