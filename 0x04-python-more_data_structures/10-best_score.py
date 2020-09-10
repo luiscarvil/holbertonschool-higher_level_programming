469ebd546a830461a616450d66f6c5e24b6716ee
luiscarvil <1888@holbertonschool.com>
2020-09-10 16:59:55 -0500

    best score in list

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    a_dic_max= None
+    a_dic_max = None
     if a_dictionary:
         a_dic_max = max(a_dictionary, key=a_dictionary.get)
     return a_dic_max
