f6ca3407910ddf37067188c2653a13efc3073b2d
luiscarvil <1888@holbertonschool.com>
2020-09-10 19:58:43 -0500

    roman numbers to int

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,7 @@
 def roman_to_int(roman_string):
     numb = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     total = 0
-    if roman_string is None and type(roman_string) != str:
+    if roman_string is None or type(roman_string) != str:
         return 0
     for i in range(len(roman_string)):
         if i > 0 and numb[roman_string[i - 1]] < numb[roman_string[i]]:
