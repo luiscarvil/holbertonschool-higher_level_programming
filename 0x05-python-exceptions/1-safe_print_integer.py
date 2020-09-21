5d5b5e57ea3d2fb5defb612620cebb1cc6464bc0
luiscarvil <1888@holbertonschool.com>
2020-09-21 13:24:41 -0500

    safe print integer

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,5 +3,5 @@ def safe_print_integer(value):
     try:
         print("{:d}".format(value))
     except ValueError:
-        print("{:s} is not an integer".format(value))
+        print("{} is not an integer".format(value))
     return value
