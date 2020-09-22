d69a0fa03fd4a92dc4d0df9358ab7a09734d1d99
luiscarvil <1888@holbertonschool.com>
2020-09-22 17:41:40 -0500

    square class

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+method square - defines a square.
+"""
+
+
+class Square:
+    """
+    class square check conditions
+    """
+    def __init__(self, size):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >=0")
+        self.__size = size
+
+    def area(self):
+        return (self.__size * self.__size)
