b3b95286d89847c5f2c518789dce1d6960f6e5be
luiscarvil <1888@holbertonschool.com>
2020-09-22 19:56:07 -0500

    print matriz with #

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""
+method square - defines a square.
+"""
+
+
+class Square:
+    """
+    class square init size
+    """
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        """
+        return the value size
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            """
+            raise error TypeError and push menssage
+            """
+            raise TypeError("size must be an integer")
+        if value < 0:
+            """
+            raise error ValueError and push menssage
+            """
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return (self.__size ** 2)
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        for i in range(self.__size):
+            for j in range(self.__size):
+                print("#", end="")
+            print()
