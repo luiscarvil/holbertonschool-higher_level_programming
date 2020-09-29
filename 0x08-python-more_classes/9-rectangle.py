af9199ac3a5f6df6810952fff801dad3d52f789f
root <root@LUISCV.localdomain>
2020-09-28 21:56:03 -0500

    add classes

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -22,8 +22,8 @@ class Rectangle:
             width (int, another): First int. Defaults to 0.
             height (int, another): Second int. Defaults to 0.
         """
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
         Rectangle.number_of_instances += 1
 
     def __str__(self):
