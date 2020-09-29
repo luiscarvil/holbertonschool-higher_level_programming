50872cb084d23476864aea817f347fb6df2db6c1
root <root@LUISCV.localdomain>
2020-09-29 18:55:24 -0500

    locked class

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -8,9 +8,4 @@ class LockedClass:
     """
     New class with no ibject attribute
     """
-    def __setattr__(self, attribute, value):
-        if attribute != "first_name":
-            raise AttributeError("'LockedClass' object has \
-no attribute 'last_name'")
-        else:
-            self.__dict__[attribute] = ValueError
+    __slots__ = ["first_name"]
