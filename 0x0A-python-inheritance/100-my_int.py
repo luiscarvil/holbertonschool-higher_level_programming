fd3052e826ba99dfb42d3fd86def7702cad57c08
luiscarvil <1888@holbertonschool.com>
2020-10-06 18:58:20 -0500

    Python - Inheritance

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" module comparitions """
+class MyInt(int):
+    """ swapped == and =! """
+    def __eq__(self, value):
+        """ return opposite """
+        return (int(self) != int(value))
+
+    def __ne__(self, value):
+        """ return opposite """
+        return(int(self) == int(value))
