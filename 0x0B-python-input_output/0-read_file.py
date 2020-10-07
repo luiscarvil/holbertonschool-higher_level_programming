0600eb4fe26d15dc2578ab03fc8c2953b42a79ac
root <root@LUISCV.localdomain>
2020-10-07 17:57:03 -0500

    read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -8,5 +8,5 @@ def read_file(filename=""):
     Args:
         filename (str, ""): [name file passed to read].
     """
-    with open('my_file_0.txt', encoding="utf-8") as f:
+    with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
