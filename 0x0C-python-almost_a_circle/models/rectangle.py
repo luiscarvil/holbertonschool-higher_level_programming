3412d1d016cff13c4321718f55d25a8c449d0a8c
root <root@LUISCV.localdomain>
2020-10-13 12:55:12 -0500

    add classes

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -86,7 +86,7 @@ class Rectangle(Base):
 
     def __str__(self):
         """ print values """
-        return ("[Rectangle] ({:d})) {:d}/{:d} - {:d}/{:d}".format(
+        return ("[Rectangle] ({:d}) {:d}/{:d} - {:d}/{:d}".format(
             self.id, self.__x, self.__y, self.__width, self.__height))
 
     def update(self, *args, **kwargs):
