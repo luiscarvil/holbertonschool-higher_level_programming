e9a080ae61bba034aaf37ec1b3f338c24dd91006
luiscarvil <1888@holbertonschool.com>
2020-11-17 20:38:43 -0500

    add task

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,4 @@
+-- Import the database dump from hbtn_0d_tvshows to your MySQL server
+SELECT tv_genres.name AS "genre", count(*) AS "number_of_shows" FROM tv_show_genres
+LEFT JOIN tv_genres ON tv_genres.id = genre_id
+GROUP BY genre_id ORDER BY count(*) DESC;
