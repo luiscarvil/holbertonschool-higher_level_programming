118250ac15d60c65eb64a4b17005ea04e77cd551
luiscarvil <1888@holbertonschool.com>
2020-12-12 15:20:51 -0500

    add files tasks Object-relational mapping

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -9,12 +9,15 @@ import sys
 from model_state import Base, State
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+        sys.argv[1],
+        sys.argv[2],
+        sys.argv[3])
+    )
 
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    new_state = State(name='Lousiana')
+    new_state = State(name='Louisiana')
     session.add(new_state)
     session.commit()
     print(new_state.id)
