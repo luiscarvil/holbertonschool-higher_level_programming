eb8ae06eff3fde142486270bf7be72bf19e2b0dc
luiscarvil <1888@holbertonschool.com>
2020-12-18 15:30:52 -0500

    project python network

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 # Bash script that takes in a URL, sends a POST request to the pa\
 ssed URL, and displays the body of the response
-curl -sd "email=hr@holbertonschool.com&subject=I%20will%20always%\
-20be%20here%20for%20PLD" "$1"
+curl -sd "email=hr@holbertonschool.com&subject=I%20will%20always%20be%20here%20for%20PLD" "$1"
