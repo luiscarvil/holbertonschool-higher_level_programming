b145f4fdf9d1d7c4ce0093122a25b3b41c834856
luiscarvil <1888@holbertonschool.com>
2020-12-18 18:16:45 -0500

    post curl json

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that sends a request to a URL passed as an argument, and displays only the status code of the response
-curl -d @"$2" -H 'Content-Type: application/json' "$1"
+curl -sd @"$2" -H 'Content-Type: application/json' "$1"
