0207f84e7abcb261a7e11253543b9c834b43d567
luiscarvil <1888@holbertonschool.com>
2021-01-11 19:40:48 +0000

    network python task

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     from sys import argv
     mail = {'email': argv[2]}
     data = parse.urlencode(mail).encode('utf-8')
-    # print(data) --> b'email=hr%40holbertonschool.com' 
+    # print(data) --> b'email=hr%40holbertonschool.com'
     req = request.Request(argv[1], data)
     # print(req) --> <urllib.request.Request object at 0x7f6b9a926fd0>
     # print(type(req)) --> <class 'urllib.request.Request'>
