b6e45ee3721d545daae4d89067e7e5bc9c624252
luiscarvil <1888@holbertonschool.com>
2021-01-11 19:19:57 +0000

    network python task

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -2,8 +2,9 @@
 """
 Response header value ( takes in a URL and sends a request to the URL)
 """
-import urllib.request
-from sys import argv
-req = urllib.request.Request(argv[1])
-with urllib.request.urlopen(req) as req:
-    print(req.headers.get("X-Request-Id"))
+if __name__ == "__main__"
+    import urllib.request
+    from sys import argv
+    req = urllib.request.Request(argv[1])
+    with urllib.request.urlopen(req) as req:
+        print(req.headers.get("X-Request-Id"))
