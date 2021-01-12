58d8b036a0b7ec6ffc9d4286ff7f41428bdadc62
luiscarvil <1888@holbertonschool.com>
2021-01-12 14:00:26 +0000

    network python task

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -6,7 +6,7 @@ decode in utf-8
 if __name__ == "__main__":
     import requests
     from sys import argv
-    print(len(argv))
+    # print(len(argv))
     req = requests.get(argv[1])
     if req.status_code >= 400:
         print("Error code: {}".format(req.status_code))
