0bd6babea5de51aeda82eefb06dc7c027354605c
luiscarvil <1888@holbertonschool.com>
2021-01-12 13:54:02 +0000

    network python task

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+what's my status task (script fetches)
+"""
+if __name__ == "__main__":
+    import requests
+    req = requests.get('https://intranet.hbtn.io/status')
+    html = req.text
+    print("Body response:")
+    print("\t- type: {}".format(type(html)))
+    print("\t- utf8 content: {}".format(html))
+
