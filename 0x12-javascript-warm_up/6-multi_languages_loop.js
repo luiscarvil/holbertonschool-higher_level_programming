5a2d0cdd60cc8c62d0532adb459ea8fe9d3a8f7a
luiscarvil <1888@holbertonschool.com>
2021-02-02 19:30:47 +0000

    loop array strings

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
-const arrayStr = ['C is fun', 'Python is cool', 'Javascript is amazing', ]
-for (let i = 0; i < arrayStr.length; i++) console.log(arrayStr[i])
+const arrayStr = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (let i = 0; i < arrayStr.length; i++) console.log(arrayStr[i]);
