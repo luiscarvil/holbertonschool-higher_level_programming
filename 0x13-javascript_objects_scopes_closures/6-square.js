09da7a25d34062c577dcd0216d85e2547ec94384
luiscarvil <1888@holbertonschool.com>
2021-02-09 01:05:37 +0000

    add javascript square task

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,13 +1,9 @@
 #!/usr/bin/node
 // Square class that inherits from the Rectangle class
 
-const Rectangle = require('./4-rectangle');
+const Rectangle = require('./5-square');
 
 module.exports = class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
-
   charPrint (c) {
     if (c == null) {
       c = 'X';
